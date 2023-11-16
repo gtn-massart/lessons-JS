53f255c9b8393df7bcebe320fec5eee38123487e
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2023-11-16 13:57:57 +0100

    Intro eventListener

--- 10.Les-évènements/1.Introduction-aux-évènements/script.js
+++ 10.Les-évènements/1.Introduction-aux-évènements/script.js
@@ -8,6 +8,25 @@
     Elle permet d'être à l'écoute d'un évenement particulier sur un élément, c'est à dire de déclencher une fonction callback quand un évènement a été déclenché sur un élément.
 */
     
+    const btn = document.querySelector('.btn');
+
+    btn.addEventListener('click', handleClick);
+    btn.addEventListener('click', handleClick2);
+
+    // ↓ !!! c'est moins manipulable avec une fonction anonyme, par exemple si on veut retirer l'évenement.
+    btn.addEventListener('click', () => console.log('clicked 3 !!!'));
+
+    function handleClick(event) {
+        console.log('Clicked !!!');
+    }
+
+    function handleClick2(event) {
+        console.log('Clicked 2 !!!');
+    }
+
+    // → Dans la console on peut vérifier les différents eventListener appliqués à un élément. Cela fonctionne UNIQUEMENT dans la console
+    // → getEventListeners(document.querySelector('.btn'));
+
 
 
 /* 
@@ -18,6 +37,12 @@
     À vous de voir si vous voulez l'utiliser, ou pas.
 
 */
+    btn.addEventListener('click', handleClick4);
+
+    function handleClick4(event) {
+        console.log(event);
+    }
+
 
 
 /* 
@@ -29,6 +54,10 @@
     Attention, on ne peut qu'enlever des écouteurs d'évènements rajoutés avec la méthode addEventListnerner ET lorsque on a passé une fonction nommée et pas une fonction anonyme.
 */
 
+    // btn.removeEventListener('click', handleClick2);
+
+    // ↓ ne fonctionnera pas car a été créé avec une fonction anonyme ()
+    // btn.removeEventListener('click', handleClick3)
 
 /* 
     Déclarer un écouteur d'évènement en ligne.
@@ -41,6 +70,8 @@
     - On ne peut pas utiliser les paramètres supplémentaires d'addEventListeners()
 */
 
+    // !!! NE PLUS UTILISER
+    // btn.onclick = handleClick;
 
 /* 
     Rajouter des évènements en HTML.
