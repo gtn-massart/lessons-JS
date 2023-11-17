8cc326c9b8b1f164ffdc5969ca16d2f6da8fa518
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2023-11-17 08:15:39 +0100

    Mouse events

--- 10.Les-évènements/5.Les-évènements-de-souris/script.js
+++ 10.Les-évènements/5.Les-évènements-de-souris/script.js
@@ -4,8 +4,17 @@
     L'objet d'évènement fourni à la callback nous donnera donc des informations spécifiques au pointage.
 */
 
+    // document.addEventListener('click', handleClick);
+    // document.addEventListener('dblclick', handleClick);
+    // document.addEventListener('mousedown', handleClick);
+    // document.addEventListener('mouseup', handleClick);
 
-
+    // function handleClick(e) {
+    //     console.log(e);
+    //     document.querySelector('.clic-info').textContent = `
+    //     Event : ${e.type}, X : ${e.pageX}, Y : ${e.pageY}
+    //     `
+    // }
 
 /* 
     Il existe également d'autres évenements en rapport avec la souris, sans le clic.
@@ -13,3 +22,13 @@
     Notamment : mouseover, mousemove, mouseout, mouseenter, mouseleave, wheel 
 */
     
+const box = document.querySelector('.box');
+const subBox = document.querySelector('.sub-box');
+
+// box.addEventListener('mousemove', () => console.log('Mousemove'));
+// box.addEventListener('mouseenter', () => console.log('Mouseenter'));
+// box.addEventListener('mouseleave', () => console.log('Mouseleave'));
+// box.addEventListener('mouseover', () => console.log('Mouseover'));
+// box.addEventListener('mouseout', () => console.log('Mouseout'));
+
+document.addEventListener('wheel', () => console.log('Roulette'));
