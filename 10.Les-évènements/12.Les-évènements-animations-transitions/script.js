0eb2eadfc9df74db6e127c7848d0070213a45c45
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2023-11-19 12:29:31 +0100

    animation and transition events

--- 10.Les-évènements/12.Les-évènements-animations-transitions/script.js
+++ 10.Les-évènements/12.Les-évènements-animations-transitions/script.js
@@ -6,3 +6,13 @@
     Animation : animationstart, animationend
 */
 
+    const box = document.querySelector('.box');
+
+    box.addEventListener('transitionstart', () => console.log('Transition start'));
+    box.addEventListener('transitionend', () => console.log('Transition end'));
+    box.addEventListener('transitionrun', () => console.log('Transition run'));
+    box.addEventListener('transitioncancel', () => console.log('Transition cancel'));
+
+    
+    box.addEventListener('animationstart', () => console.log('animation start'));
+    box.addEventListener('animationend', () => console.log('animation end'));
