df2302a828736c07882af03c372ab9170c774342
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2023-11-19 12:13:22 +0100

    video and audio events

--- 10.Les-évènements/10.Les-évènements-des-médias/script.js
+++ 10.Les-évènements/10.Les-évènements-des-médias/script.js
@@ -5,9 +5,22 @@
     Découvrons ensemble : pause, play, loadeddata, playing, progress, etc...
 */
 
+    const video = document.querySelector('video');
 
+    video.addEventListener('loadeddata', () => console.log("Data loaded"));
+    video.addEventListener('play', () => console.log("Play"));
+    video.addEventListener('pause', () => console.log("Pause"));
+    video.addEventListener('timeupdate', () => console.log("time update"));
+    video.addEventListener('volumechange', () => console.log("Volume changed"));
 
 /* 
     Même combat pour l'élément <audio>.
 */
 
+    const audio = document.querySelector('audio');
+
+    audio.addEventListener('loadeddata', () => console.log("Data loaded"));
+    audio.addEventListener('play', () => console.log("Play"));
+    audio.addEventListener('pause', () => console.log("Pause"));
+    audio.addEventListener('timeupdate', () => console.log("time update"));
+    audio.addEventListener('volumechange', () => console.log("Volume changed"));
