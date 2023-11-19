bdf4f7522dfca5f303608e3ba3aa63fa8111bab6
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2023-11-19 12:56:33 +0100

    exo event submit

--- 10.Les-évènements/Exercices/1-Exercice-évènements-I/script.js
+++ 10.Les-évènements/Exercices/1-Exercice-évènements-I/script.js
@@ -1,3 +1,21 @@
 /*
     Prévenez le comportement par défaut du formulaire afin de récupérer les données écrites dans les inputs et les afficher dans les paragraphes correspondants.
 */
+
+const form = document.querySelector('form');
+const inputs = document.querySelectorAll('form input')
+const firstnameText = document.querySelector('.firstname');
+const lastnameText = document.querySelector('.lastname');
+
+console.log(inputs[1]);
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    firstnameText.innerText = `${firstnameText.innerText} ${inputs[0].value}`;
+    lastnameText.innerText = `${lastnameText.innerText} ${inputs[1].value}`;
+
+    form.reset();
+}
