dd9ffb4dde4abc02d22671300a48e1bfeaa8c006
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2023-11-19 12:33:53 +0100

    load events

--- 10.Les-évènements/13.Les-évènements-de-chargement/script.js
+++ 10.Les-évènements/13.Les-évènements-de-chargement/script.js
@@ -1,3 +1,7 @@
 /*
     Voici les évènements concernant le chargement d'une page : DOMContentLoaded, load.
 */
+
+    document.addEventListener('DOMContentLoaded', () => console.log("Construction du Dom terminé"));
+
+    window.addEventListener('load', () => console.log("Données chargées"))
