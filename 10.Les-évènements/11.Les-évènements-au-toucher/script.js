7f2c5d58c13abceb823695011d361b0200ed0b6e
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2023-11-19 12:22:04 +0100

    touch events

--- 10.Les-évènements/11.Les-évènements-au-toucher/script.js
+++ 10.Les-évènements/11.Les-évènements-au-toucher/script.js
@@ -2,3 +2,8 @@
     Les "touch events" permettent de prendre en compte le "clic" d'un doigt sur un écran tactile.
 */
 
+    // !!! POUR TESTER, PASSER EN TOGGLE DEVICE TOOLBAR DANS LA CONSOLE !!!
+
+    document.addEventListener('touchstart', () => console.log('touchstart'));
+    document.addEventListener('touchend', () => console.log('touchend'));
+    document.addEventListener('touchmove', () => console.log('touchmove'));
