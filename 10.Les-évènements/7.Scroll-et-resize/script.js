90a9c17ac365358f5273ea3d2fcd975eb0376689
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2023-11-19 11:12:20 +0100

    scroll and resize

--- 10.Les-évènements/7.Scroll-et-resize/script.js
+++ 10.Les-évènements/7.Scroll-et-resize/script.js
@@ -2,10 +2,20 @@
     L'évènement scroll se déclenche quand le contenu de l'élément sur lequel il est attaché subit un scroll.
 */
 
+  const info = document.querySelector('.info');
 
+  document.addEventListener('scroll', handleScroll);
+
+  function handleScroll(e) {
+    console.log('scroll', e);
+    info.textContent = `Body ${window.scrollY}`
+  }
 
 /*
   L'évènement resize est disponible seulement sur l'objet window.
   Il se déclenche quand on réduit la fenêtre.  
 */
 
+  window.addEventListener('resize', () => console.log('RESIZED !!!!!'));  
+  console.log(window.innerHeight);
+  console.log(window.innerWidth);
