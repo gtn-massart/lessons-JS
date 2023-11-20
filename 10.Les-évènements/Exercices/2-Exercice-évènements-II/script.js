3e7afe9b72f16dcbf31b68c804d34de00caa500e
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2023-11-20 09:46:53 +0100

    Exo 2 mousemove + transform: translate

--- 10.Les-évènements/Exercices/2-Exercice-évènements-II/script.js
+++ 10.Les-évènements/Exercices/2-Exercice-évènements-II/script.js
@@ -2,3 +2,16 @@
     Faites-en sorte que le cercle suive votre souris.
     Le pointeur de votre souris doit se trouver parfaitement au milieu du cercle.
 */
+
+const circle= document.querySelector('.circle');
+console.log(circle);
+
+window.addEventListener('mousemove', handleCircle);
+
+function handleCircle(e) {
+
+   console.log(`${e.pageX}px, ${e.pageY}px`);
+   
+   circle.style.transform = `translate(calc(${e.pageX}px - 50%), calc(${e.pageY}px - 50%))`
+
+}
