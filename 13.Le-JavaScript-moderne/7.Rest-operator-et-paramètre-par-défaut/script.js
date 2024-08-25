ec4fef60c361d502f05188cd4c2091533a4cbea8
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2024-08-25 09:15:08 +0200

    Rest operator and default params

--- 13.Le-JavaScript-moderne/7.Rest-operator-et-paramètre-par-défaut/script.js
+++ 13.Le-JavaScript-moderne/7.Rest-operator-et-paramètre-par-défaut/script.js
@@ -3,10 +3,26 @@
     Le paramètre du reste permet d'accepter un nombre indéfini d'arguments sous forme d'un tableau.
 */
 
+function concat(...strings) {
+    console.log(strings);
+    let fullString = "";
+    for(const string of strings) {
+        fullString += string; 
+    }
+    return fullString    
+}
+// console.log(concat("Bonjour", "le", "monde" ));
 
 /* 
     2. Les paramètres par défaut.
 
     Ils permettent de définir une valeur par défaut si un argument correspondant au paramètre en question n'est pas fourni.
 */
+
+function welcoming(userName = 'visiteur') {
+    return `Bienvenue, ${userName}`;
+}
+console.log(welcoming());
+console.log(welcoming("Gaëtan"));
+console.log(welcoming("David"));
 
