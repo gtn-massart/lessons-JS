82f18d74c771202d35b2f8c4e94da0a4254cf84c
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2024-08-26 14:39:47 +0200

    le décompte

--- 14.Le-JavaScript-asynchrone/Exercices/1.Le-décompte/script.js
+++ 14.Le-JavaScript-asynchrone/Exercices/1.Le-décompte/script.js
@@ -1,1 +1,21 @@
 
+const text = document.querySelector("h2")
+
+function countdownValue() {
+
+  const now = new Date()
+  const countdownDate = new Date("September 1, 2024")
+  const distance =countdownDate - now
+  console.log(distance);  
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+  const secondes = Math.floor((distance % (1000 * 60)) / 1000)
+
+  console.log(days, hours, minutes,secondes);
+
+  text.textContent = `${days}j ${hours}h ${minutes}m ${secondes}s`
+}
+
+setInterval(countdownValue, 1000)
