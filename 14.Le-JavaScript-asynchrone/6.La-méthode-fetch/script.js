95ed5a3b6ccb466768454021e60f561d2038c233
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2024-08-26 13:38:11 +0200

    La méthode fetch

--- 14.Le-JavaScript-asynchrone/6.La-méthode-fetch/script.js
+++ 14.Le-JavaScript-asynchrone/6.La-méthode-fetch/script.js
@@ -1,2 +1,24 @@
 /* Découvrons la méthode fetch en JavaScript */
 
+// https://api.thecatapi.com/v1/images/search
+
+function getCatImg(url) {
+
+  fetch(url)
+  .then(response => {
+    console.log(response)
+    return response.json()    
+  })
+  .then(data => {
+    console.log(data);
+    createImg(data)
+  })
+}
+
+function createImg(imgData) {
+  const img = document.createElement("img")
+  img.src = imgData[0].url 
+  document.body.appendChild(img)   
+}
+
+getCatImg("https://api.thecatapi.com/v1/images/search")
