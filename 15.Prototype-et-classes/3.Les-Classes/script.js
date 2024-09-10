073f07831bf1a89b74894fa0eee8706096db3103
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2024-09-10 09:39:48 +0200

    Les classes

--- 15.Prototype-et-classes/3.Les-Classes/script.js
+++ 15.Prototype-et-classes/3.Les-Classes/script.js
@@ -6,3 +6,21 @@
     Le terme "sucre syntaxique" est beaucoup utilisé pour définir ce genre d'ajout. 
 */
 
+class Planet{
+    constructor(name, radius, color){
+        this.name = name
+        this.radius = radius
+        this.color= color
+    }
+    // ↓ le fonction spin() s'ajoute au prototype
+    spin(){
+        console.log("spinning");        
+    }
+}
+
+console.log(typeof Planet);
+console.dir(Planet)
+
+const mars = new Planet("Mars", 3400, "Red")
+console.log(mars);
+mars.spin()
