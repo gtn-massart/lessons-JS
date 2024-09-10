66b314b299540c776de15af6cc5651149e3b58c5
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2024-09-10 11:02:32 +0200

    Exercice classe

--- 15.Prototype-et-classes/Exercices/2.Exercices-Classes/script.js
+++ 15.Prototype-et-classes/Exercices/2.Exercices-Classes/script.js
@@ -7,8 +7,29 @@
     3. channelType (array)
 
     Cette classe intègre également deux méthodes dans son prototype :
-    1. 'deleteAcount', qui console.log("Channel deleted")
+    1. 'deleteAccount', qui console.log("Channel deleted")
     2. 'renameChannel', qui peut changer le nom de la chaîne 
 
 */
+
+class YoutubeChannel {
+    constructor(channelName, totalViews, channelType) {
+        this.channelName = channelName
+        this.totalViews = totalViews
+        this.channelType = channelType
+    }
+    deleteAccount(){
+        console.log("Channel deleted");       
+    }
+    renameChannel(newChannelName) {
+        this.channelName = newChannelName             
+    }
+}
+
+const myChannel = new YoutubeChannel("My Channel 1", 2000, ['gaming', 'history', 'console'])
+
+console.log(myChannel);
+myChannel.deleteAccount()
+myChannel.renameChannel("My Channel 2")
+console.log(myChannel.channelName);
 
