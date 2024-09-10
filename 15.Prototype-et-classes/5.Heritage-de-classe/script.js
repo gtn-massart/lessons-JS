9997d880d91e05a0456f0d7031d2fbe64546f5c0
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2024-09-10 10:29:06 +0200

    héritage de classe

--- 15.Prototype-et-classes/5.Heritage-de-classe/script.js
+++ 15.Prototype-et-classes/5.Heritage-de-classe/script.js
@@ -4,3 +4,35 @@
     C'est le même fonctionnement que l'héritage de prototype des fonctions constructeur que nous avons vu précédemment, et c'est la même chose qui se passe sous le capot.
 */
 
+class Human {
+    constructor(arm, leg){
+        this.arm = arm
+        this.leg = leg
+    }
+    walk(){
+        console.log("Walk");  
+    }
+    think(){
+        console.log("think");  
+    }
+}
+
+const human1 = new Human(2, 2)
+console.log(human1);
+
+class Italian extends Human {
+    constructor(arm, leg, name){
+        super(arm, leg)
+        this.name = name
+    }
+    cookingPasta(){
+        console.log("🍝");  
+    }
+    cookingPizza(){
+        console.log("🍕");  
+    }
+}
+
+const italian1 = new Italian(2, 2, "Gaetano Massaro")
+console.log(italian1);
+ 
